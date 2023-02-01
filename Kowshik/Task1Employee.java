e73135f15d5c40658e92d73d00c7e8f0f01a9de7
notthefakekowsik <kowsik02@gmail.com>
2023-02-01 11:52:51 +0530

    kowshikFirstCommit

---
+++ Kowshik/Task1Employee.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Task1Employee
+{
+    private String name;
+    private int age;
+    private String location;
+}
---
+++ Kowshik/Task1Main.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Task1Main
+{
+    public static void main(String args[])
+    {
+        List<Task1Employee> listOfEmployees = new ArrayList<>();
+        listOfEmployees.add(new Task1Employee("Kowshik",25,"Vijayawada"));
+        listOfEmployees.add(new Task1Employee("Hari" , 27,  "Kanchikacharla"));
+        listOfEmployees.add(new Task1Employee("Roja" , 28,"Hyd"));
+        listOfEmployees.add(new Task1Employee("Elon" , 60 ,"mars"));
+        listOfEmployees.add(new Task1Employee("virat",32,"Delhi"));
+        listOfEmployees.add(new Task1Employee("dhawan" , 35,"Delhi"));
+
+         Map<String,List<Task1Employee>> filteredEmployeesData;
+
+         List<Task1Employee> currList = listOfEmployees.stream().filter(x -> x.getAge() >= 25 && x.getAge() <= 40).collect(Collectors.toList());
+
+        filteredEmployeesData = currList.stream().collect(Collectors.groupingBy(Task1Employee :: getLocation));
+        System.out.println(filteredEmployeesData.size());
+        System.out.println();
+        System.out.println(filteredEmployeesData);
+
+         for(String name : filteredEmployeesData.keySet()) {
+             System.out.print(name+"->");
+             for (Task1Employee curr : filteredEmployeesData.get(name)) {
+                 System.out.println(curr.getName()+" "+curr.getAge());
+             }
+             System.out.println();
+         }
+    }
+}
